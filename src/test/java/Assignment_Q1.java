1b86d5ef0872f17c84e708e3e8d4e8b89431c853
mohai <mohai.cse@gmail.com>
2025-04-12 06:18:40 +0600

    Assignment 1st commit

---
+++ src/test/java/Assignment_Q1.java
@@ -0,0 +1,130 @@
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.DataProvider;
+import org.testng.annotations.Test;
+
+import java.io.IOException;
+import java.time.DayOfWeek;
+import java.time.Duration;
+import java.time.LocalDate;
+import java.util.List;
+
+
+public class Assignment_Q1 {
+
+    WebDriver driver;
+    String filePath = "./src/test/resources/Excel.xlsx";
+
+
+    LocalDate date = LocalDate.now();
+    DayOfWeek dayOfWeek = date.getDayOfWeek();
+    String sheetName = dayOfWeek.toString().trim();
+    int startingPointRow = 2;
+
+    @BeforeClass
+    public void setup(){
+
+        driver = new ChromeDriver();
+        driver.manage().deleteAllCookies();
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+        driver.get("https://www.google.com/");
+        driver.manage().window().maximize();
+
+    }
+
+    @Test(dataProvider = "MyData")
+    public void TC001_MyTest(String data) throws IOException {
+
+        WebElement searchBox = driver.findElement(By.xpath("//textarea[@id='APjFqb']"));
+        searchBox.clear();
+        searchBox.sendKeys(data);
+
+        List<WebElement> searchList = driver.findElements(By.xpath("//div[@id='Alh6id']//ul[@role='listbox']/li"));
+
+        String shortestText = Assignment_Q1.shortestOption(searchList);
+        String longestText = Assignment_Q1.longestOption(searchList);
+
+        System.out.println(shortestText);
+        System.out.println(longestText);
+
+        ExcelUtility excelUtility = new ExcelUtility(filePath);
+
+        excelUtility.setCellData(sheetName,startingPointRow,3,longestText);
+        excelUtility.setCellData(sheetName,startingPointRow,4,shortestText);
+
+        startingPointRow++;
+
+    }
+
+    @AfterClass
+    public void tearDown(){
+        driver.quit();
+    }
+
+    @DataProvider(name="MyData")
+    private String[] dataProvider() throws IOException {
+
+        ExcelUtility utility = new ExcelUtility(filePath);
+
+        int rowCount = utility.getNumberOfRow(sheetName);
+        String[] data = new String[rowCount];
+
+        for(int i=2; i<=rowCount; i++){
+
+            data[i-2] = utility.getCellData(sheetName,i,2);
+
+        }
+
+        return data;
+    }
+
+    public static String longestOption(List<WebElement> elementList){
+
+        String longText = "";
+        int count = 0;
+        for(WebElement element: elementList){
+
+            String text = element.getText();
+            int textLen = text.length();
+
+            if(textLen>count){
+
+                longText = text;
+                count = textLen;
+
+            }
+
+
+        }
+
+        return longText;
+
+    }
+
+    public static String shortestOption(List<WebElement> elementList){
+
+        String shortText = "";
+        int count = Integer.MAX_VALUE;
+
+        for(WebElement element: elementList){
+
+            String text = element.getText();
+            int textLen = text.length();
+
+            if(textLen<count){
+
+                shortText = text;
+                count = textLen;
+
+            }
+
+        }
+
+        return shortText;
+    }
+
+}
---
+++ src/test/java/ExcelUtility.java
@@ -0,0 +1,78 @@
+import org.apache.poi.xssf.usermodel.XSSFCell;
+import org.apache.poi.xssf.usermodel.XSSFSheet;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
+
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+
+public class ExcelUtility {
+
+    public FileInputStream fi;
+    public FileOutputStream fo;
+    public XSSFWorkbook workbook;
+    public XSSFSheet sheet;
+    public XSSFCell cell;
+    public String filePath;
+
+
+    public ExcelUtility(String path){
+        filePath = path;
+
+    }
+
+    public int getNumberOfRow(String sheetName) throws IOException {
+
+        fi = new FileInputStream(filePath);
+        workbook = new XSSFWorkbook(fi);
+        sheet = workbook.getSheet(sheetName);
+        int rowCount = sheet.getLastRowNum();
+        workbook.close();
+        fi.close();
+
+        return rowCount;
+    }
+
+    public String getCellData(String sheetName, int rowNumber, int cellNumber) throws IOException {
+
+        fi = new FileInputStream(filePath);
+        workbook = new XSSFWorkbook(fi);
+        sheet = workbook.getSheet(sheetName);
+
+        String cellData;
+
+        try {
+            cell = sheet.getRow(rowNumber).getCell(cellNumber);
+            cellData = cell.toString();
+        } catch (Exception e) {
+            cellData = "";
+        }
+
+        workbook.close();
+        fi.close();
+
+        return cellData;
+    }
+
+    public void setCellData(String sheetName, int rowNumber, int cellNumber, String data) throws IOException {
+
+        fi = new FileInputStream(filePath);
+        workbook = new XSSFWorkbook(fi);
+        sheet = workbook.getSheet(sheetName);
+
+        cell = sheet.getRow(rowNumber).createCell(cellNumber);
+        cell.setCellValue(data);
+
+        fo = new FileOutputStream(filePath);
+        workbook.write(fo);
+
+        fo.close();
+        workbook.close();
+        fi.close();
+
+
+    }
+
+
+
+}
